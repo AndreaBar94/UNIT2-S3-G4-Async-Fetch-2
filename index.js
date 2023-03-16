dc26323edfa7c345719132b9b330796986f4568a
AndreaBar94 <122469003+AndreaBar94@users.noreply.github.com>
2023-03-16 16:54:38 +0100

    edit search js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 //load images
 let btn = document.getElementById("loadBtn");
 let btn2 = document.getElementById("load2ndBtn");
+let searchbar = document.getElementById("searchbar").value;
+let searchBtn = document.getElementById("searchBtn");
 
 let getImg = async () => {
 	try {
@@ -81,3 +83,42 @@ let getImg2 = async () => {
 	}
 };
 getImg2();
+
+let getNewImages = async () => {
+	try {
+		let loadedImg = await fetch("https://api.pexels.com/v1/search?query=" + searchbar, {
+			headers: new Headers({
+				Authorization: "gPDGxuK3N0faAq0l1IS4BFo1aOBkRp6MbfCWNDFi5bQT6OqORIZu7GII",
+			}),
+		});
+		let elements = await loadedImg.json();
+		let photos = elements.photos;
+		console.log(photos);
+
+		searchBtn.addEventListener("click", () => {
+			let cards = document.querySelectorAll(".card");
+			cards.forEach((card, index) => {
+				let photo = photos[index];
+				let svgElement = document.querySelector(".card-img-top");
+				let imgElement = document.createElement("img");
+				imgElement.setAttribute("src", photo.src.large);
+				imgElement.setAttribute("alt", photo.alt);
+				svgElement.replaceWith(imgElement);
+				let title = card.querySelector(".card-title");
+				title.innerHTML = photo.alt;
+				let photographer = card.querySelector(".card-text");
+				photographer.innerHTML = `This photograph was taken by ${photo.photographer}`;
+				let small = card.querySelector("small");
+				small.textContent = `${photo.id}`;
+
+				let deleteBtn = card.querySelector(".btn-group button:nth-child(2)");
+				deleteBtn.textContent = "Hide";
+				deleteBtn.addEventListener("click", () => {
+					card.remove();
+				});
+			});
+		});
+	} catch (error) {
+		console.log(error);
+	}
+};
